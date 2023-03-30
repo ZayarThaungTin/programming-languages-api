7dbb20e9323ff89df14a9ce748d739bed071dfd7
Zayar Thaung Tin <zayarthaungtin22394@gmail.com>
2023-03-30 13:11:03 +0630

    Add existing project files to Git

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "db4free.net",
+      user: "zayarthaungtin",
+      password: "X9sQ2bp56cSjr!P",
+      database: "the_king_db",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
